5968b4b255f93b50b546fba6800e90ac3b74582a
PatricioPoncini <patricioponcini10@gmail.com>
2023-09-11 19:35:39 -0300

    update private constructor

--- server/src/domain/entities/Visitor.ts
+++ server/src/domain/entities/Visitor.ts
@@ -3,7 +3,7 @@ export class Visitor {
     ip: string;
     nickname: string;
 
-    constructor(id: string, ip: string, nickname: string) {
+    private constructor(id: string, ip: string, nickname: string) {
         this.id = id;
         this.ip = ip;
         this.nickname = nickname;
