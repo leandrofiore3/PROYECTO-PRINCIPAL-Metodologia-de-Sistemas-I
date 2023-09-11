d727f1d35fc7cea6025aab5359f0176ddd063b9a
Mariela Farina <mariela.l.farina1@gmail.com>
2023-09-11 20:45:05 -0300

    add claim repository

---
+++ server/src/infrastructure/repositories/claim-repository.ts
@@ -0,0 +1,20 @@
+import Claim from "../../domain/entities/claim.entity.ts";
+class ClaimRepository{
+    private claims: Claim[];
+    public constructor(){
+        this.claims=[];
+    }
+    public async save(claim: Claim): Promise<void>{
+        const saveClaim = this.claims.find(a=> a.getId()=== claim.getId());
+        if (saveClaim){
+            this.claims.splice(this.claims.indexOf(saveClaim),1);
+        }
+        this.claims.push(claim);
+    }
+    public async findOneById(id: string): Promise<Claim | null>{
+        const claim= this.claims.find(a=> a.getId()=== id);
+        return claim ? claim : null;
+    }
+
+}
+export default new ClaimRepository();
