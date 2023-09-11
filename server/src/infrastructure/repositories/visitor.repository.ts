0783619b5e15f8a39188c9a926f379ccded961eb
leandrofiore3 <leandrofiore3@gmail.com>
2023-09-11 20:30:51 -0300

    add findOneById method

--- server/src/infrastructure/repositories/visitor.repository.ts
+++ server/src/infrastructure/repositories/visitor.repository.ts
@@ -9,12 +9,15 @@ class VisitorRepository {
 
   async save(visitor: Visitor): Promise<void> {
     const savedVisitor = this.visitors.find(u => u.getId() === visitor.getId())
-
     if (savedVisitor) {
       this.visitors.splice(this.visitors.indexOf(savedVisitor), 1)
     }
-
     this.visitors.push(visitor);
+  }
+
+  async findOneById(id: string): Promise<Visitor | null> {
+    const visitor = this.visitors.find(u => u.getId() === id);
+    return (visitor) ? visitor : null;
   }
 
 }
