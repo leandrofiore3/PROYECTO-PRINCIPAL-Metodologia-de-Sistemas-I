5aaecf7afdd8ee09e4d21a0f999a505e67814493
PatricioPoncini <patricioponcini10@gmail.com>
2023-09-25 20:52:59 -0300

    add category seeder

--- server/src/domain/entities/category.entity.ts
+++ server/src/domain/entities/category.entity.ts
@@ -12,5 +12,9 @@ class Category {
     getId(): string {
         return this.id;
     }
+
+    static create(id: string, name: string, color: string): Category {
+        return new Category(id, name, color);
+    }
 }
 export default Category
---
+++ server/src/infrastructure/seeders/seeder.category.ts
@@ -0,0 +1,21 @@
+import Category from "../../domain/entities/category.entity";
+import categoryRepository from "../repositories/category.repository";
+
+class CategorySeeder {
+    private categories: Array<Category> = [];
+
+    public constructor() {
+        this.categories.push(Category.create("1", "First category", "#26577C"));
+        this.categories.push(Category.create("1", "Second category", "#E55604"))
+        this.categories.push(Category.create("1", "Third category", "#B4B4B3"))
+        this.categories.push(Category.create("1", "Fourth category", "#EBE4D1"))
+    }
+
+    public async generate(): Promise<void> {
+        for (const category of this.categories) {
+            await categoryRepository.save(category);
+        }
+    }
+}
+
+export default new CategorySeeder();
