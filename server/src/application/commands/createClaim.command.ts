6c60617a9e321879732261e46ee8921490b0db52
PatricioPoncini <patricioponcini10@gmail.com>
2023-10-02 19:45:20 -0300

    add create claim command

---
+++ server/src/application/commands/createClaim.command.ts
@@ -0,0 +1,68 @@
+import Category from "../../domain/entities/category.entity";
+import Claim from "../../domain/entities/claim.entity";
+import Visitor from "../../domain/entities/visitor.entity";
+
+class CreateClaimCommand {
+    private readonly id: string;
+    private readonly owner: Visitor;
+    private readonly title: string;
+    private readonly description: string;
+    private readonly category: Category;
+    private readonly location: string;
+    private readonly createdAt: Date;
+    private readonly cloneOf?: Claim;
+
+    constructor(
+        id: string,
+        owner: Visitor,
+        title: string,
+        description: string,
+        category: Category,
+        location: string,
+        createdAt: Date,
+        cloneOf?: Claim
+    ) {
+        this.id = id;
+        this.owner = owner;
+        this.title = title;
+        this.description = description;
+        this.category = category;
+        this.location = location;
+        this.createdAt = createdAt;
+        this.cloneOf = cloneOf;
+    }
+
+    public getId(): string {
+        return this.id;
+    }
+
+    public getOwner(): Visitor {
+        return this.owner;
+    }
+
+    public getTitle(): string {
+        return this.title;
+    }
+
+    public getDescription(): string {
+        return this.description;
+    }
+
+    public getCategory(): Category {
+        return this.category;
+    }
+
+    public getLocation(): string {
+        return this.location;
+    }
+
+    public getCreatedAt(): Date {
+        return this.createdAt;
+    }
+
+    public getCloneOf(): Claim | undefined {
+        return this.cloneOf;
+    }
+}
+
+export default CreateClaimCommand;
