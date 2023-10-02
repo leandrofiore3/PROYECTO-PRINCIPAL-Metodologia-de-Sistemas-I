3257e12058ddffae3d41f4562919f59a57b5bd60
carreras gaston <gast.andres@gmail.com>
2023-10-02 20:16:38 -0300

    dislike-command

--- server/src/application/commands/DislikeCommand.ts
+++ server/src/application/commands/DislikeCommand.ts
@@ -1,36 +1,18 @@
 import Visitor from "../../domain/entities/visitor.entity";
-import Category from "../../domain/entities/category.entity";
-import Claim from "../../domain/entities/claim.entity"; // Importa la entidad Claim si aún no lo has hecho.
+import Claim from "../../domain/entities/claim.entity";
 
 class DislikeCommand {
   private readonly id: string;
   private readonly owner: Visitor;
-  private readonly title: string;
-  private readonly description: string;
-  private readonly category: Category;
-  private readonly location: string;
-  private readonly createAt: Date;
-  private readonly cloneOf: Claim | null;
   private dislikes: number;
 
   constructor(
     id: string,
     owner: Visitor,
-    title: string,
-    description: string,
-    category: Category,
-    location: string,
-    createAt: Date,
-    cloneOf: Claim | null
+
   ) {
     this.id = id;
     this.owner = owner;
-    this.title = title;
-    this.description = description;
-    this.category = category;
-    this.location = location;
-    this.createAt = createAt;
-    this.cloneOf = cloneOf;
     this.dislikes = 0;
   }
 
@@ -42,30 +24,7 @@ class DislikeCommand {
     return this.owner;
   }
 
-  public getTitle(): string {
-    return this.title;
-  }
-
-  public getDescription(): string {
-    return this.description;
-  }
-
-  public getCategory(): Category {
-    return this.category;
-  }
-
-  public getLocation(): string {
-    return this.location;
-  }
-
-  public getCreateAt(): Date {
-    return this.createAt;
-  }
-
-  public getCloneOf(): Claim | null {
-    return this.cloneOf;
-  }
-
+  
   public getDislikes(): number {
     return this.dislikes;
   }
