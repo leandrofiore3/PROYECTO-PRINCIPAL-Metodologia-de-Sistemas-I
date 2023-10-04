73f9b2c76c19cdd6293644799733c51bfb8025eb
carreras gaston <gast.andres@gmail.com>
2023-10-03 21:25:40 -0300

    agregando validacion Pin de Visitor

--- server/src/application/handlers/DislikeHandler.ts
+++ server/src/application/handlers/DislikeHandler.ts
@@ -1,7 +1,7 @@
-import {Request, Response} from 'express';
-import { DislikeCommand } from '../../commands/DislikeCommand.js';
-import ClaimRepository from '../../infrastructure/repositories/claim-repository.js'; // Asegúrate de importar el ClaimRepository desde el archivo adecuado.
-
+import { Request, Response } from "express";
+import { DislikeCommand } from "../../commands/DislikeCommand.js";
+import ClaimRepository from "../../infrastructure/repositories/claim-repository.js"; // Asegúrate de importar el ClaimRepository desde el archivo adecuado.
+import Visitor from "../../domain/entities/visitor.entity.js";
 class DislikeHandler {
   private claimRepository: typeof ClaimRepository;
 
@@ -9,15 +9,22 @@ class DislikeHandler {
     this.claimRepository = claimRepository;
   }
 
-  public async execute(command: DislikeCommand): Promise<void> {
+  public async execute(
+    command: DislikeCommand,
+    visitor: Visitor
+  ): Promise<void> {
     const claimId = command.getClaimId();
     const claim = await this.claimRepository.findOneById(claimId);
 
     if (!claim) {
-      throw new Error('Claim does not exist');
+      throw new Error("Claim does not exist");
+    }
+    const providedPin = command.getVisitorPin();
+    if (providedPin !== visitor.getPin()) {
+      throw new Error("Invalid PIN");
     }
 
-    claim.dislike(); 
+    claim.dislike();
 
     await this.claimRepository.save(claim);
   }
