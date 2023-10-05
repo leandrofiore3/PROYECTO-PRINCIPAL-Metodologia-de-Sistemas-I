05039d86345e67d4687a42f9baf53b34044b1cc1
Mariela Farina <mariela.l.farina1@gmail.com>
2023-10-05 15:13:26 -0300

    add sisLike-Action

---
+++ server/src/http/actions/dislike-action.ts
@@ -0,0 +1,36 @@
+import { Request, Response } from 'express';
+import DislikeCommand from '../../application/commands/DislikeCommand'; 
+import DislikeHandler from '../../application/handlers/DislikeHandler'; 
+import Visitor from '../../domain/entities/visitor.entity'; 
+
+class DislikeAction {
+  private readonly dislikeHandler: DislikeHandler;
+
+  constructor(dislikeHandler: DislikeHandler) {
+    this.dislikeHandler = dislikeHandler;
+  }
+
+  public async run(req: Request, res: Response) {
+    const { claimId, pin } = req.body; 
+
+    try {
+      if (!claimId || !pin) {
+        return res.status(400).json({ message: 'Claim ID and PIN are required' });
+      }
+
+      // Aquí podrías agregar lógica adicional para autenticar al visitante si es necesario.
+
+      const visitor = new Visitor(); 
+      const command = new DislikeCommand(claimId, visitor, pin);
+
+      await this.dislikeHandler.execute(command, visitor);
+
+      return res.status(200).json({ message: 'Disliked successfully' });
+    } catch (error) {
+      const { message } = error as Error;
+      res.status(400).json({ message });
+    }
+  }
+}
+
+export default DislikeAction;
