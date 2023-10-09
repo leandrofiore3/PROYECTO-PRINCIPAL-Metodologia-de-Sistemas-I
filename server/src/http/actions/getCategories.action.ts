acb658697e6a024df7b076941b158aedb043b521
PatricioPoncini <patricioponcini10@gmail.com>
2023-10-09 20:46:26 -0300

    add categories action

--- server/src/http/actions/getCategories.action.ts
+++ server/src/http/actions/getCategories.action.ts
@@ -1,17 +1,17 @@
 import { Request, Response } from 'express';
-import CategoryService from '../category.service'; // ver 
+import categoryRepository from 'infrastructure/repositories/category.repository';
 class GetCategoriesAction {
-  public async run(req: Request, res: Response) {
-    try {
-      
-      const categories = await CategoryService.getCategories();
-     
-      return res.status(200).json(categories);
-    } catch (error) {
-      
-      return res.status(500).json({ error: 'Error al obtener las categorías.' });
+    public async run(req: Request, res: Response) {
+        try {
+
+            const categories = await categoryRepository.findAll();
+
+            return res.status(200).json(categories);
+        } catch (error) {
+
+            return res.status(500).json({ error: 'Error al obtener las categorías.' });
+        }
     }
-  }
 }
 export default new GetCategoriesAction();
 
--- server/src/http/routes/SetUpCategories.routes.ts
+++ server/src/http/routes/SetUpCategories.routes.ts
@@ -1,5 +1,6 @@
 import { Application } from 'express';
 import CommonRoutes from './common.routes';
+import getCategoriesAction from 'http/actions/getCategories.action';
 // importar el getcategoriesaction
 
 class CategoriesRoutes extends CommonRoutes {
