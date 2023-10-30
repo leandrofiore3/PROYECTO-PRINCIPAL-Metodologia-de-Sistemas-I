b31c3861c726069b36694d270da5502a4b63c0f5
leandrofiore3 <leandrofiore3@gmail.com>
2023-10-30 18:52:26 -0300

    get last claims action creada

---
+++ server/src/http/actions/getLastClaimsAction.ts
@@ -0,0 +1,17 @@
+import { Request, Response } from 'express';
+import claimRepository from 'infrastructure/repositories/claim-repository';
+
+class GetLastClaimsAction {
+    public async run(_req: Request, res: Response) {
+        try {
+
+            const lastClaims = await claimRepository.findLast5Claims();
+
+            res.status(200).json(lastClaims);
+        } catch (error) {
+
+            res.status(500).json({ error: 'Error al obtener los reclamos más recientes.' });
+        }
+    }
+}
+export default new GetLastClaimsAction();
