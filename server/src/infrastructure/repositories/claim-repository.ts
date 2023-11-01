0f0ffe95a01f55cd9214d931631e88d7db4ffc0d
Joaquin Sosa <129340097+JoaquinSosa2607@users.noreply.github.com>
2023-11-01 19:51:55 -0300

    add find last 5 claims method

--- server/src/infrastructure/repositories/claim-repository.ts
+++ server/src/infrastructure/repositories/claim-repository.ts
@@ -6,16 +6,22 @@ class ClaimRepository {
         this.claims = [];
     }
     public async save(claim: Claim): Promise<void> {
-        const saveClaim = this.claims.find(a => a.getId() === claim.getId());
+        const saveClaim = this.claims.find((a) => a.getId() === claim.getId());
         if (saveClaim) {
             this.claims.splice(this.claims.indexOf(saveClaim), 1);
         }
         this.claims.push(claim);
     }
     public async findOneById(id: string): Promise<Claim | null> {
-        const claim = this.claims.find(a => a.getId() === id);
+        const claim = this.claims.find((a) => a.getId() === id);
         return claim ? claim : null;
     }
-
+    public async findLast5Claims(): Promise<Claim[]> {
+        const sortedClaims = this.claims.sort((a, b) => {
+            return b.createAt.getTime() - a.createAt.getTime();
+        });
+        const last5Claims = sortedClaims.slice(0, 5);
+        return last5Claims;
+    }
 }
 export default new ClaimRepository();
