5c0d5586940de9d86186ab417e937e3f92372ec2
PatricioPoncini <patricioponcini10@gmail.com>
2023-11-06 20:25:48 -0300

    fix save in claim repository

--- server/src/infrastructure/repositories/claim-repository.ts
+++ server/src/infrastructure/repositories/claim-repository.ts
@@ -7,9 +7,9 @@ class ClaimRepository {
   }
 
   public async save(claim: Claim): Promise<void> {
-    const saveClaim = this.claims.find((a) => { a.getId() === claim.getId() });
+    const saveClaim = this.claims.find((a) => a.getId() === claim.getId());
     if (saveClaim) {
-      this.claims.splice(this.claims.indexOf(saveClaim), 1);
+      this.claims.slice(this.claims.indexOf(saveClaim), 1);
     }
     this.claims.push(claim);
   }
